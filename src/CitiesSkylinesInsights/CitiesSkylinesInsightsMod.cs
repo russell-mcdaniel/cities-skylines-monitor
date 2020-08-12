ae9bac70f96d8f02413dbc66ce87834772a8e1f5
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2020-08-11 21:40:42 -0700

    Create basic mod project

---
+++ src/CitiesSkylinesInsights/CitiesSkylinesInsightsMod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ICities;
+
+namespace CitiesSkylinesInsights
+{
+    public class CitiesSkylinesInsightsMod : IUserMod
+    {
+        public string Name => "Insights";
+
+        public string Description => "Gain insights from game activity";
+    }
+}
