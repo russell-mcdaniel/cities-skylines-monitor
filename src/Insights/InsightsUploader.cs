1e4dfbff23172a02adcba6b39196445edd408aaf
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2021-03-02 22:55:08 -0800

    Create upload module

--- src/Insights/InsightsUploader.cs
+++ src/Insights/InsightsUploader.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Threading;
 using Insights.Logging;
+using Insights.Utilities;
 
 namespace Insights
 {
@@ -15,6 +18,13 @@ namespace Insights
         private const string UploaderErrorMessage = "The uploader encountered an error.";
 
         protected InsightsLogger Logger { get; } = new InsightsLogger(typeof(InsightsUploader));
+
+#if DEBUG
+        //private readonly Uri _uploadUri = new Uri("https://localhost:44300/upload");
+        private readonly Uri _uploadUri = new Uri("http://localhost:55300/upload");
+#else
+        private readonly Uri _uploadUri = new Uri("https://www.siliconesis.com/cities-skylines/insights/api/v1.0/upload");
+#endif
 
         private readonly string _archiveDirectory = $"{LogFileManager.GetLogFileDirectory(LogFileType.Game)}Archive";
         private Regex _fileNameExpression = new Regex(FileNamePattern);
@@ -29,6 +39,9 @@ namespace Insights
         {
             // Create the archive directory.
             Directory.CreateDirectory(_archiveDirectory);
+
+            // Configure server certificate validation.
+            ServicePointManager.ServerCertificateValidationCallback = ProcessLogFileUploadServerCertificate;
         }
 
         public void Start()
@@ -138,7 +151,86 @@ namespace Insights
         {
             Logger.LogDebug($"{nameof(ProcessLogFileUpload)} > Uploading log file.");
 
-            Thread.Sleep(250);
+            try
+            {
+                var json = ProcessLogFileUploadCreateRequestJson(filePath);
+
+                Logger.LogDebug($"{nameof(ProcessLogFileUpload)} > Request JSON: {json}");
+
+                var request = (HttpWebRequest)WebRequest.Create(_uploadUri);
+                request.ContentType = "application/json";
+                request.Method = WebRequestMethods.Http.Post;
+
+                using (var stream = request.GetRequestStream())
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(json);
+                }
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    Logger.LogDebug($"{nameof(ProcessLogFileUpload)} > Request response code is {response.StatusCode}.");
+                }
+
+                return true;
+            }
+            catch (WebException ex)
+            {
+                Logger.LogError($"{nameof(ProcessLogFileUpload)} > Unable to upload log file.", ex);
+
+                return false;
+            }
+        }
+
+        private string ProcessLogFileUploadCreateRequestJson(string filePath)
+        {
+            Logger.LogDebug($"{nameof(ProcessLogFileUploadCreateRequestJson)} > Creating request JSON.");
+
+            var fileName = Path.GetFileName(filePath);
+            var fileContent = File.ReadAllText(filePath);
+            var fileContentEscaped = JsonHelper.Escape(fileContent);
+
+            var json = $"{{ \"FileName\": \"{fileName}\", \"FileContent\": \"{fileContentEscaped}\" }}";
+
+            return json;
+        }
+
+        /// <summary>
+        /// Process server certificate validation.
+        /// </summary>
+        /// <remarks>
+        /// Support for secure requests has been deferred. This method remains to support future work.
+        /// </remarks>
+        private bool ProcessLogFileUploadServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors policyErrors)
+        {
+            Logger.LogDebug($"{nameof(ProcessLogFileUploadServerCertificate)} > Validating server certificate.");
+
+            if (policyErrors != SslPolicyErrors.None)
+            {
+                var message = string.Empty;
+
+                if ((policyErrors & SslPolicyErrors.RemoteCertificateNotAvailable) == SslPolicyErrors.RemoteCertificateNotAvailable)
+                {
+                    message = "The server did not present a certificate.";
+                }
+                else
+                {
+                    if ((policyErrors & SslPolicyErrors.RemoteCertificateNameMismatch) == SslPolicyErrors.RemoteCertificateNameMismatch)
+                    {
+                        message = "The server certificate name does not match the domain name.";
+                    }
+
+                    if ((policyErrors & SslPolicyErrors.RemoteCertificateChainErrors) == SslPolicyErrors.RemoteCertificateChainErrors)
+                    {
+                        if (!string.IsNullOrEmpty(message))
+                            message += " ";
+
+                        message += "The server certificate chain has additional problems.";
+                    }
+                }
+
+                Logger.LogWarn($"{nameof(ProcessLogFileUploadServerCertificate)} > The server certificate could not be validated. {message}");
+            }
 
             return true;
         }
---
+++ src/Insights/Utilities/JsonHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace Insights.Utilities
+{
+    /// <summary>
+    /// Provides JSON handling services.
+    /// </summary>
+    /// <remarks>
+    /// No JavaScript or JSON helpers, such as System.Web.HttpUtility.JavaScriptStringEncode, are
+    /// available in the release of Mono used by Cities: Skylines. Other resources were consulted
+    /// to create a routine to escape text for JSON string properties:
+    /// 
+    /// https://www.json.org/
+    /// https://stackoverflow.com/questions/1242118/how-to-escape-json-string
+    /// https://stackoverflow.com/questions/19176024/how-to-escape-special-characters-in-building-a-json-string
+    /// </remarks>
+    public static class JsonHelper
+    {
+        public static string Escape(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < json.Length; i++)
+            {
+                var c = json[i];
+
+                switch (c)
+                {
+                    case '"':
+                    case '\\':
+                    case '/':
+                        builder.Append('\\');
+                        builder.Append(c);
+                        break;
+
+                    case '\b':
+                        builder.Append("\\b");
+                        break;
+
+                    case '\f':
+                        builder.Append("\\f");
+                        break;
+
+                    case '\n':
+                        builder.Append("\\n");
+                        break;
+
+                    case '\r':
+                        builder.Append("\\r");
+                        break;
+
+                    case '\t':
+                        builder.Append("\\t");
+                        break;
+
+                    default:
+                        if (c < ' ')
+                        {
+                            builder.Append("\\u" + ((int)c).ToString("X4", System.Globalization.CultureInfo.InvariantCulture));
+                        }
+                        else
+                        {
+                            builder.Append(c);
+                        }
+                        break;
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
